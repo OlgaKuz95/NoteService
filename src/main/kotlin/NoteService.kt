153d91309cbbcfe8aca58415ee7ad45fc6d114ce
Olga Kuzevanova <cuzevanova.olg@yandex.ru>
2021-06-10 01:41:54 +0700

    add Noteinter

--- src/main/kotlin/NoteService.kt
+++ src/main/kotlin/NoteService.kt
@@ -1,28 +1,86 @@
 package ru.netology
 
-
-object  NoteService{
-    //private var comments = List<Comment>()
-    //private var notes = List<Note>()
-
-    private fun <T> List(String:T, Int:T) {}
+import ru.netology.ru.netology.noteAccessToTheCommentException
+import ru.netology.ru.netology.noteIdNotFoundException
 
 
-    private var id = 0
+object NoteService {
+    val notes = mutableListOf<Note>()
+    val comments = mutableListOf<Comment>()
+    var noteId = 0
+    var commentId = 0
+
+    fun createComment(comment: Comment) {
+        comments += comment
+        if (notes.any { noteId >= 0 }) {
+            println("$commentId.add")
+        } else (throw noteIdNotFoundException("0"))
+    }
+
+    fun add(note: Note): Note {
+        notes += note.copy(noteId = noteId++)
+        return notes.last()
+    }
+
+    //fun read(comment: Comment) {}
+
+    fun delete(note: Note) {
+        notes += note
+        ("notes.any { noteId >=0 } is  ${notes.any { noteId >= 0 }}")
+        println("1")
+        ("notes.any { noteId>=0}  !is  ${notes.any { noteId >= 0 }}")
+        (throw noteIdNotFoundException("0"))
+    }
+
+    fun deleteComment(comment: Comment) {
+        comments += comment
+        ("commentId.any { commentId >=0 } is  ${comments.any { commentId >= 0 }}")
+        println("01")
+        ("commentId.any { commentId>=0}  !is  ${notes.any { commentId >= 0 }}")
+        (throw noteAccessToTheCommentException("02"))
+    }
+
+    fun edit(note: Note): Boolean {
+        notes += note
+        ("notes.any { noteId >=0 } is  ${notes.any { noteId >= 0 }}")
+        println(note.copy())
+        ("notes.any { noteId>=0}  !is  ${notes.any { noteId >= 0 }}")
+        return false
+    }
+
+    fun editComment(comment: Comment): Boolean {
+        comments += comment
+        println("commentId.any { commentId >=0 } is  ${comments.any { commentId >= 0 }}")
+        true
+        println("commentId.any { commentId>=0}  !is  ${notes.any { commentId >= 0 }}")
+        return false
+    }
 
 
-
-fun create(comment: Comment) {
-
-
-        }
-    fun read(comment: Comment){}
-
-
-
-
-
-
+    val note = Note(
+        1,
+        "text",
+        "te",
+        "all",
+        "all",
+        1,
+        2,
+        1,
+        3,
+        1,
+        1,
+    )
+    val comment = Comment(
+        1,
+        1,
+        "t",
+        1,
+        1,
+        0,
+        1,
+        2,
+        1,
+    )
 
 
 }
